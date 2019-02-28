31eef907862fe8d33157df811eccfad8020cd083
vsh <vsh@adaperio.ru>
2019-02-28 18:41:57 +0300

    some funny logic added (and unimportant logging)
    just for some fun and experience

--- HoroscopeFromAWebPage/Form1.Designer.cs
+++ HoroscopeFromAWebPage/Form1.Designer.cs
@@ -30,13 +30,15 @@
         {
             this.buttonLoad = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.mainTextBox = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.horoscopeLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // buttonLoad
             // 
-            this.buttonLoad.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.buttonLoad.Location = new System.Drawing.Point(170, 435);
+            this.buttonLoad.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonLoad.Location = new System.Drawing.Point(170, 49);
             this.buttonLoad.Name = "buttonLoad";
             this.buttonLoad.Size = new System.Drawing.Size(120, 32);
             this.buttonLoad.TabIndex = 0;
@@ -47,34 +49,54 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Segoe Script", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Font = new System.Drawing.Font("Verdana", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label1.Location = new System.Drawing.Point(13, 22);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(272, 31);
+            this.label1.Size = new System.Drawing.Size(289, 23);
             this.label1.TabIndex = 1;
-            this.label1.Text = "Your horoscope for today:";
+            this.label1.Text = "Your urlToHoroscope for today:";
             // 
-            // textBox1
+            // mainTextBox
             // 
-            this.textBox1.Location = new System.Drawing.Point(19, 56);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.ReadOnly = true;
-            this.textBox1.Size = new System.Drawing.Size(266, 373);
-            this.textBox1.TabIndex = 2;
+            this.mainTextBox.Location = new System.Drawing.Point(17, 100);
+            this.mainTextBox.Multiline = true;
+            this.mainTextBox.Name = "mainTextBox";
+            this.mainTextBox.ReadOnly = true;
+            this.mainTextBox.Size = new System.Drawing.Size(266, 235);
+            this.mainTextBox.TabIndex = 2;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(17, 73);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // horoscopeLabel
+            // 
+            this.horoscopeLabel.AutoSize = true;
+            this.horoscopeLabel.Location = new System.Drawing.Point(17, 54);
+            this.horoscopeLabel.Name = "horoscopeLabel";
+            this.horoscopeLabel.Size = new System.Drawing.Size(98, 13);
+            this.horoscopeLabel.TabIndex = 4;
+            this.horoscopeLabel.Text = "your sign of zodiac:";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(302, 479);
-            this.Controls.Add(this.textBox1);
+            this.ClientSize = new System.Drawing.Size(302, 349);
+            this.Controls.Add(this.horoscopeLabel);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.mainTextBox);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.buttonLoad);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Horoscope for today";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -84,7 +106,9 @@
 
         private System.Windows.Forms.Button buttonLoad;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox mainTextBox;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label horoscopeLabel;
     }
 }
 
--- HoroscopeFromAWebPage/Form1.cs
+++ HoroscopeFromAWebPage/Form1.cs
@@ -9,26 +9,114 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using NLog;
 
 namespace HoroscopeFromAWebPage
 {
     public partial class Form1 : Form
     {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+        private string urlToHoroscope;
+
+        public enum Signs
+        {
+            Aries,
+            Taurus,
+            Gemini,
+            Cancer,
+            Leo,
+            Virgo,
+            Libra,
+            Scorpio,
+            Sagittarius,
+            Capricorn,
+            Aquarius,
+            Pisces
+        }
+
         public Form1()
         {
+            logger.Info("Starting app");
             InitializeComponent();
+
+            comboBox1.Items.Add(Signs.Aries);
+            comboBox1.Items.Add(Signs.Taurus);
+            comboBox1.Items.Add(Signs.Gemini);
+            comboBox1.Items.Add(Signs.Cancer);
+            comboBox1.Items.Add(Signs.Leo);
+            comboBox1.Items.Add(Signs.Virgo);
+            comboBox1.Items.Add(Signs.Libra);
+            comboBox1.Items.Add(Signs.Scorpio);
+            comboBox1.Items.Add(Signs.Sagittarius);
+            comboBox1.Items.Add(Signs.Capricorn);
+            comboBox1.Items.Add(Signs.Aquarius);
+            comboBox1.Items.Add(Signs.Pisces);
         }
 
         private void buttonLoad_Click(object sender, EventArgs e)
         {
-            WebClient wbClient = new WebClient();
-            wbClient.Encoding = Encoding.UTF8;
-            string page = wbClient.DownloadString("https://horo.mail.ru/prediction/capricorn/today/");
-            string name = "<p>(.*?)</p>";
-            var matches = Regex.Matches(page, name);
-            foreach (Match match in matches)
+            try
             {
-                 textBox1.AppendText(match.Groups[1].Value + Environment.NewLine + Environment.NewLine);
+                WebClient wbClient = new WebClient { Encoding = Encoding.UTF8 };
+                string horoscope = wbClient.DownloadString(urlToHoroscope);
+                logger.Info("Horoscope was downloaded succesfully");
+                string name = "<p>(.*?)</p>";
+                var matches = Regex.Matches(horoscope, name);
+                foreach (Match match in matches)
+                {
+                    mainTextBox.AppendText(match.Groups[1].Value + Environment.NewLine + Environment.NewLine);
+                }
+            }
+            catch (Exception exception)
+            {
+                logger.Error(exception, "App logic error");
+            }           
+            LogManager.Flush();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0:
+                    urlToHoroscope = "https://horo.mail.ru/prediction/aries/today/";
+                    break;
+                case 1:
+                    urlToHoroscope = "https://horo.mail.ru/prediction/taurus/today/";
+                    break;
+                case 2:
+                    urlToHoroscope = "https://horo.mail.ru/prediction/gemini/today/";
+                    break;
+                case 3:
+                    urlToHoroscope = "https://horo.mail.ru/prediction/cancer/today/";
+                    break;
+                case 4:
+                    urlToHoroscope = "https://horo.mail.ru/prediction/leo/today/";
+                    break;
+                case 5:
+                    urlToHoroscope = "https://horo.mail.ru/prediction/virgo/today/";
+                    break;
+                case 6:
+                    urlToHoroscope = "https://horo.mail.ru/prediction/libra/today/";
+                    break;
+                case 7:
+                    urlToHoroscope = "https://horo.mail.ru/prediction/scorpio/today/";
+                    break;
+                case 8:
+                    urlToHoroscope = "https://horo.mail.ru/prediction/sagittarius/today/";
+                    break;
+                case 9:
+                    urlToHoroscope = "https://horo.mail.ru/prediction/capricorn/today/";
+                    break;
+                case 10:
+                    urlToHoroscope = "https://horo.mail.ru/prediction/aquarius/today/";
+                    break;
+                case 11:
+                    urlToHoroscope = "https://horo.mail.ru/prediction/pisces/today/";
+                    break;
+                default:
+                    logger.Error("Switch case wasn't identified");
+                    break;
             }
         }
     }
